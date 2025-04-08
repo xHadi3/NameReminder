56b586152bcd18c97a8c616b6b28149c1760432f
xHadi3 <hadiessam9@gmail.com>
2025-04-08 21:01:59 +0300

    Initial Commit

---
+++ NameReminder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NameReminder
+//
+//  Created by Hadi Al zayer on 10/10/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NameReminder/NameReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  NameReminderApp.swift
+//  NameReminder
+//
+//  Created by Hadi Al zayer on 10/10/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct NameReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
